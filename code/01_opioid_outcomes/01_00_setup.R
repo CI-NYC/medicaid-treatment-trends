63bef8945f3f592b5c5c7bf6ea432927e2bf1e8e
Anton Hung <amh2389@ip-172-25-96-227.ec2.internal>
2024-04-13 01:18:08 +0000

    changes to mme and days covered calculations to include a larger cohort. needed to remake several scripts using the larger cohort

---
+++ code/01_opioid_outcomes/01_00_setup.R
@@ -0,0 +1,32 @@
+# -------------------------------------
+# Script: 01_00_setup
+# Author: Anton Hung
+# Purpose:
+# Notes:
+# -------------------------------------
+
+library(arrow)
+library(tidyverse)
+library(lubridate)
+library(data.table)
+library(furrr)
+library(tidylog)
+library(tictoc)
+library(doFuture)
+
+load_dir <- "/mnt/general-data/disability/create_cohort/"
+
+cohort <- readRDS(paste0(load_dir, "final/analysis_cohort.rds"))
+setDT(cohort)
+
+cohort <- cohort[, c("BENE_ID",
+                     "washout_start_dt",
+                     "washout_cal_end_dt",
+                     "washout_12mos_end_dt",
+                     "dem_race_cond",
+                     "disability_pain_12mos_cal",
+                     "opioid_pain_washout_12mos_cal"
+)]
+
+
+
